09c4cc646539b324e9cba404e3ff875b417c8707
Roman Schmitz <roman.schmitz@gmail.com>
2025-06-03 12:26:48 +0000

    Added installer for chaos mesh.

---
+++ chaos-mesh-tests/setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+helm repo add chaos-mesh https://charts.chaos-mesh.org
+helm repo update
+
+kubectl create ns chaos-mesh
+
+helm install chaos-mesh chaos-mesh/chaos-mesh \
+  -n=chaos-mesh --set chaosDaemon.runtime=containerd \
+  --set chaosDaemon.socketPath=/run/k3s/containerd/containerd.sock
+
+kubectl apply -f chaosmesh-dashboard-ingressroute.yaml
+kubectl apply -f rbac.yaml
+
+kubectl create token account-cluster-manager-jhfrk
